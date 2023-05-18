5f8a5c28fcb613b9fc515dce3b957a2f2af96f98
Thiago C Santos <106815401+thiagocsantosdev@users.noreply.github.com>
2023-05-17 21:09:15 -0300

    Update scripts.js
    Brasil
    - Adicionado comentários.
    -Adicionado novas validações para o rolamento da página
    
    U.S.A
    - Added comments.
    -Added new validations for page scrolling

--- scripts.js
+++ scripts.js
@@ -1,33 +1,52 @@
-function simular(){
- 
+// Recebe os valores solicitados e aplica uma taxa de juros fixa
+
+function simular() {
+
+ // A taxa fixa que será calculada, o valor inserido pelo usario, e a parcela solicitada
   const juros = 16
-  const valorSolicitado = parseFloat(document.getElementById('valorSolicitado').value) 
-  const parcelaSolicitada = parseFloat( document.getElementById('parcelaSolicitada').value)
- 
- 
+  const valorSolicitado = parseFloat(document.getElementById('valorSolicitado').value)
+  const parcelaSolicitada = parseFloat(document.getElementById('parcelaSolicitada').value)
+
+// Recebe as ID's do formulario para serem manipuladas 
   const solicitado = document.getElementById('displayValorSolicitado')
   const parcelamento = document.getElementById('displayParcelaSolicitada')
-  
-  
   const totalComJuros = document.getElementById('totalJuros')
   const valorAcrescimo = document.getElementById('valorAcrescimo')
   const valorParcela = document.getElementById('valorParcela')
 
-  const acrescimo = (valorSolicitado*juros)/100
-  const resultado = valorSolicitado+acrescimo
-  const parcelaMensal = resultado/parcelaSolicitada
+ // Recebe o ID para o efeito de rolagem da pagina (HREF)
+ const rolammento = document.getElementById('rolamento')
+ 
+// Calculo
+  const acrescimo = (valorSolicitado * juros) / 100
+  const resultado = valorSolicitado + acrescimo
+  const parcelaMensal = resultado / parcelaSolicitada
 
-  
-  solicitado.type="text"
-  parcelamento.type="text"
-  valorAcrescimo.type="text"
-  totalComJuros.type="text"
-  valorParcela.type="text"
+//Validacao, evita que o usuario solicte um valor menor que 100 e maior que 100.000,00
+ if (isNaN(valorSolicitado) || valorSolicitado < 100 || valorSolicitado > 100000000){
+    window.alert('O valor disponivel é entre 100 e 100.000,00')
+    rolammento.href=""
+  }
+  else{
 
-  solicitado.value = "R$:"+ valorSolicitado
-  parcelamento.value= parcelaSolicitada+"x"
-  valorAcrescimo.value="R$:"+ acrescimo
-  totalComJuros.value="R$:"  +resultado
-  valorParcela.value="R$:" +parcelaMensal.toFixed(2)
-  
+  // Adiciona o link, caso fosse colocado antes a pagina rolaria mesmo com a negação dos valores acima    
+  rolammento.href="#containerResultado"
+ 
+  // Estes elementos no formulário estão como number, evitando input de letras, foi mudado para text para fazer a contatenação com o "R$" 
+ 
+  solicitado.type = "text"
+  parcelamento.type = "text"
+  valorAcrescimo.type = "text"
+  totalComJuros.type = "text"
+  valorParcela.type = "text"
+
+  solicitado.value = "R$:" + valorSolicitado
+  parcelamento.value = parcelaSolicitada + "x"
+  valorAcrescimo.value = "R$:" + acrescimo
+  totalComJuros.value = "R$:" + resultado
+  valorParcela.value = "R$:" + parcelaMensal.toFixed(2)
+ 
+  }
+
+
 }
